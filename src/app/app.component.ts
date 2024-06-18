9231a6bbfa6d7a6e8d84e3422eb1bc7da0f03c25
Cesar Narciso <cesarpazol1029@gmail.com>
2024-06-18 15:04:59 -0600

    Practice done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularCalculator';
+  title = 'Employees!';
 }
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+    selector: "app-employee",
+    standalone: true,
+    imports: [FormsModule, CommonModule,],
+    templateUrl: "./employee.component.html"
+
+})
+
+
+export class EmployeeComponent {
+
+    private _employees: Employee[];
+
+    constructor(){
+
+        this._employees = [];
+    }
+
+    createEmployee(name: string, salary: number): void{
+
+        this._employees.push(
+
+            new Employee(name, salary)
+        );
+
+        alert(this._employees);
+    }
+
+    get employees(): Employee[]{
+
+        return this._employees;
+    }
+}
+
+class Employee {
+
+    private _name: string;
+    private _salary: number;
+
+    constructor(name: string, salary: number){
+
+        this._name = name;
+        this._salary = salary;
+    }
+
+    get name(): string{
+
+        return this._name;
+    }
+
+    set name(name: string){
+
+        this._name = name;
+    }
+
+    get salary(): number{
+
+        return this._salary;
+    }
+
+    set salary(salary: number){
+
+        this._salary = salary;
+    }
+}
